a5654453577ed867282f0a329925960676a7a5ff
Rafa morey <rafaelmreyes@hotmail.com>
2022-09-05 14:16:48 -0700

    testing server por index

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ router(app)
 app.use('/app', express.static('public'))
 
 const port = 3002
-app.listen(port)
+app.listen()
 console.log(`el servidor esta escuchando en http://localhost:${port}`)
 
 // const key = 'mongodb+srv://rafaelDiinpec:Mr178910@tc.kshjevt.mongodb.net/?retryWrites=true&w=majority'
