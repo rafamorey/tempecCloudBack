b38465055c701073b3891a01daf640446857c4f9
Rafa morey <rafaelmreyes@hotmail.com>
2022-09-27 13:57:12 -0700

    deleteOne instead of findOneAndRemove

--- components/device/store.js
+++ components/device/store.js
@@ -21,7 +21,7 @@ async function getDeviceStatusById(device){
 }
 
 async function deleteDevice(device){
-  const deviceDeleted = await Model.findOneAndRemove(device)
+  const deviceDeleted = await Model.deleteOne(device)
   return deviceDeleted
 }
 
